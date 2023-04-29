96d5cd97deb2cd20933e1300805643c14e1dccaa
romanb123 <44100462+romanb123@users.noreply.github.com>
2023-04-29 19:37:13 +0300

    Update functions.php

--- Backend/functions.php
+++ Backend/functions.php
@@ -11,13 +11,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$owner_first_name = $_POST['owner-first-name'] ?? '';
-$owner_last_name = $_POST['owner-last-name'] ?? '';
-$apartment_address = $_POST['apartment-address'] ?? '';
-$renting_start = isset($_POST['renting-start']) ? strtotime($_POST['renting-start']) : time();
-$renting_end = isset($_POST['renting-end']) ? strtotime($_POST['renting-end']) : strtotime('+1 year');
-$renting_rating = $_POST['renting-rating'] ?? 3;
-
+$owner_first_name = $_POST['owner_first_name'] ?? '';
+$owner_last_name = $_POST['owner_last_name'] ?? '';
+$apartment_address = $_POST['apartment_address'] ?? '';
+$renting_start = isset($_POST['renting_start']) ? date('Y-m-d', strtotime($_POST['renting_start'])) : date('Y-m-d', time());
+$renting_end = isset($_POST['renting_end']) ? date('Y-m-d', strtotime($_POST['renting_end'])) : date('Y-m-d', strtotime('+1 year'));
+$renting_rating = $_POST['renting_rating'] ?? 3;
 
 
 $owner_first_name = mysqli_real_escape_string($conn, $owner_first_name);
@@ -26,6 +25,7 @@ $apartment_address = mysqli_real_escape_string($conn, $apartment_address);
 $renting_start = mysqli_real_escape_string($conn, $renting_start);
 $renting_end = mysqli_real_escape_string($conn, $renting_end);
 $renting_rating = mysqli_real_escape_string($conn, $renting_rating);
+
 
 $sql = "INSERT INTO rent_info (owner_first_name, owner_last_name, apartment_address, renting_start, renting_end, renting_rating)
 VALUES ('$owner_first_name', '$owner_last_name', '$apartment_address', '$renting_start', '$renting_end', '$renting_rating')";
